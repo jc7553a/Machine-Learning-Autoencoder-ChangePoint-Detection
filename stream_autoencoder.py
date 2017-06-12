263501dca805a40681b6c9e8dfa27b246ad7b426
JP <jc7553a@student.american.edu>
2017-06-12 16:53:16 -0400

    Added Shuttle Testing Data and new Autoencoder

--- stream_autoencoder.py
+++ stream_autoencoder.py
@@ -1,23 +1,33 @@
 import tensorflow as tf
 import numpy as np
 import pylab
-import random
+from random import randint
+#import scipy
 from matplotlib.pylab import *
+
 
 def normalize(train):
     shape = np.shape(train)
-    print(shape)
-    for i in range (shape[1]):
-        mean = 0
-        for k in range(shape[0]):
-            mean += train[k][i]
-        mean = mean/float(shape[0])
-        std = np.std(train[:] [i])
-        for j in range(shape[0]):
-            train[j][i] = round(float((train[j][i] -mean)),3)
+    train = np.array(train)
+    i = 0
+    while i < (shape[1]):
+        mean = np.average(train[0:shape[0]][:])
+        stdDev = np.std(train[0:shape[0]][:])
+        y = 0
+        length = shape[0]
+        while y < length:
+            absolute = np.absolute(train[y][i])
+            if absolute > mean +2*stdDev:
+                train = np.delete(train, y, 0) 
+                length -= 1
+                y = -1
+            y += 1
+        shape = np.shape(train)
+        i +=1
     return train
 
 
+print("Training on Class 1 ")
 mat =[]
 for line in open('shuttleTrain1.txt').readlines():
     holder = line.split(' ')
@@ -26,60 +36,148 @@ for line in open('shuttleTrain1.txt').readlines():
             holder[i] = float(holder[i])
     mat.append(holder)
 
-Ones = []
+
+
+
+
+Fours = []
 for j in range(len(mat)):
     if mat[j][9] == 1:
-        Ones.append(mat[j][:])
+        Fours.append(mat[j][:])
+        
+print(np.shape(Fours))
+Fours.sort(key=lambda row: row[0:])
+Fours = normalize(Fours)
+print(np.shape(Fours))
+print(Fours[0:2][:])
 
 
-Ones.sort(key=lambda row: row[0:])
-print(Ones[5344][:])
-Ones = np.delete(Ones, [0,9],1)
 
-
-shuttleData = np.array(Ones).astype('float32')
+#Ones = normalize(Ones)
+Fours = np.delete(Fours, [0,9],1)
+#Fours = normalize(Fours)
+shuttleData = np.array(Fours).astype('float32')
 shape = np.shape(shuttleData)
 print(shape)
 
-'''
 mat2 =[]
-for line in open('shuttleTest.txt').readlines():
+for line in open('shuttleTest1.txt').readlines():
     holder = line.split(' ')
     i = 0
     for i in range (len(holder)):
             holder[i] = float(holder[i])
     mat2.append(holder)
 
-mat2 = np.delete(mat2, 9, 1)
-#normTest = normalize(mat2)
-shuttleTest = np.array(mat2).astype('float32')
-shuttleTest = shuttleTest[0:1000][:]
-shapeTest = np.shape(shuttleTest)
+class1 = []
+class2 = []
+class3 = []
+class4 = []
+class5 = []
+class6 = []
+class7 = []
+
+for w in range(len(mat2)):
+    if mat2[w][9] == 1:
+        class1.append(mat2[w][:])
+    if mat2[w][9] == 2:
+        class2.append(mat2[w][:])
+    if mat2[w][9] == 3:
+        class3.append(mat2[w][:])
+    if mat2[w][9] == 4:
+        class4.append(mat2[w][:])
+    if mat2[w][9] == 5:
+        class5.append(mat2[w][:])
+    if mat2[w][9] == 6:
+        class6.append(mat2[w][:])
+    if mat2[w][9] == 7:
+        class7.append(mat2[w][:])
+
+class1.sort(key=lambda row: row[0:])
+class2.sort(key=lambda row: row[0:])
+class3.sort(key=lambda row: row[0:])
+class4.sort(key=lambda row: row[0:])
+class5.sort(key=lambda row: row[0:])
+class6.sort(key=lambda row: row[0:])
+class7.sort(key=lambda row: row[0:])
+
+
+class1 = np.delete(class1, [0,9], 1)
+class2 = np.delete(class2, [0,9],1)
+class3 = np.delete(class3, [0,9], 1)
+class4 = np.delete(class4, [0,9],1)
+class5 = np.delete(class5, [0,9], 1)
+class6 = np.delete(class6, [0,9], 1)
+class7 = np.delete(class7, [0,9], 1)
+
+
+shapeTest = np.shape(class1)
+shapeTest2 = np.shape(class2)
+shapeTest3 = np.shape(class3)
+shapeTest4 = np.shape(class4)
+shapeTest5 = np.shape(class5)
+shapeTest6 = np.shape(class6)
+shapeTest7 = np.shape(class7)
+
+shuttleTest = np.array(class1[0:shapeTest[0]][:]).astype('float32')
+shuttleTest2 = np.array(class2[0:shapeTest2[0]][:]).astype('float32')
+shuttleTest3 = np.array(class3[0:shapeTest3[0]][:]).astype('float32')
+shuttleTest4 = np.array(class4[0:shapeTest4[0]][:]).astype('float32')
+shuttleTest5 = np.array(class5[0:shapeTest5[0]][:]).astype('float32')
+shuttleTest6 = np.array(class6[0:shapeTest6[0]][:]).astype('float32')
+shuttleTest7 = np.array(class7[0:shapeTest7[0]][:]).astype('float32')
+
+#print(scipy.stats.zscore(shuttleTest6))
 '''
+shuttleTest = np.array(class1[0:100][:]).astype('float32')
+shuttleTest2 = np.array(class2[0:100][:]).astype('float32')
+shuttleTest3 = np.array(class3[0:100][:]).astype('float32')
+shuttleTest4 = np.array(class4[0:100][:]).astype('float32')
+shuttleTest5 = np.array(class5[0:100][:]).astype('float32')
+shuttleTest6 = np.array(class6[0:3][:]).astype('float32')
+shuttleTest7 = np.array(class7[0:3][:]).astype('float32')
 
-
+shapeTest4 = np.shape(shuttleTest4)
+shapeTest3 = np.shape(shuttleTest3)
+shapeTest5 = np.shape(shuttleTest5)
+shapeTest = np.shape(shuttleTest)
+shapeTest2 = np.shape(shuttleTest2)
+shapeTest6 = np.shape(shuttleTest6)
+shapeTest7 = np.shape(shuttleTest7)
+'''
 
 
 #print(shapeTraining)
 n_hidden = 5
+n_hidden2 = 2
+n_hidden3 = 5
 n_features = shape[1]
 
 
 
 x = tf.placeholder(tf.float32 , [None , n_features], name = 'x')
+#learnRate = tf.placeholder(tf.float32, shape = [], name = 'LR')
 
 'Weights and Biases to Hidden Layer'
 
-w = tf.Variable(tf.random_normal([n_features ,n_hidden]), name = 'weights_h')
-b = tf.Variable(tf.random_normal([n_hidden]), name = 'biases_h')
+w = tf.Variable(tf.truncated_normal([n_features ,n_hidden], stddev = .001), name = 'weights_h')
+b = tf.Variable(tf.truncated_normal([n_hidden], stddev = .001), name = 'biases_h')
+'''
+'Weights and Biases to Hidden Layer2'
+w2 = tf.Variable(tf.random_normal([n_hidden ,n_hidden2]), name = 'weights_h2')
+b2 = tf.Variable(tf.random_normal([n_hidden2]), name = 'biases_h2')
 
-
+'Weights and Biases to Hidden Layer 3'
+w3 = tf.Variable(tf.random_normal([n_hidden2 ,n_hidden3]), name = 'weights_h3')
+b3 = tf.Variable(tf.random_normal([n_hidden3]), name = 'biases_h3')
+'''
 'Weights and Biases to Output Layer'
-wo = tf.Variable(tf.random_normal([n_hidden, n_features]), name = 'weights_o')
+wo = tf.Variable(tf.truncated_normal([n_hidden, n_features],stddev = .001), name = 'weights_o')
 bo = tf.Variable(tf.random_normal([n_features]), name = 'biases_o')
 
 'Calculations for Encoder and Decoder'
 encoder = tf.nn.sigmoid(tf.matmul(x, w) + b)
+#hidden1 = tf.nn.sigmoid(tf.matmul(encoder, w2)+b2)
+#hidden2 = tf.nn.sigmoid(tf.matmul(hidden1, w3)+b3)
 decoder = tf.matmul(encoder, wo) +bo
 y = decoder
 
@@ -87,10 +185,10 @@ y = decoder
 
 'Objective Functions'
 y_true = tf.placeholder(tf.float32, [None, n_features], name = 'y_true')
-init = tf.global_variables_initializer()
 loss = tf.reduce_mean(tf.square(y_true - y))
-optimizer = tf.train.GradientDescentOptimizer(.5)
-train = optimizer.minimize(loss)
+optimizer = tf.train.GradientDescentOptimizer(.00002).minimize(loss)
+train2 = tf.train.GradientDescentOptimizer(.05).minimize(loss)
+#train = optimizer.minimize(loss)
 
 'Set up For batch training if needed'
 
@@ -98,10 +196,9 @@ train = optimizer.minimize(loss)
 #print(tf.get_default_graph().as_graph_def())
 
 losses = []
-mini_epochs = 100
-batch_size = 25
-total_epochs = 350
-buffer = [[None]*shape[1]]*batch_size
+mini_epochs = 400
+batch_size = 5
+total_epochs = 1500
 
 print("\nNumber of Epochs: ", total_epochs)
 print("Batch Size: ", batch_size)
@@ -109,145 +206,202 @@ print("\n")
 
 
 'Load Graph'
+init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
+print(sess.run(w))
 
 train_losses = []
 print("Start Training....")
+batchTot = []
+#batchRate = np.float32(.000002)
+for t in range(200):
+    #print("New Batch")
+    batch_losses = []
+    for y in range(mini_epochs):
+        rando = randint(0,shape[0]-5)
+        batch_xs = shuttleData[rando:rando+batch_size][0:shape[1]]
+        batch_ys = shuttleData[(rando):(rando+batch_size)][0:shape[1]]
+        sess.run(optimizer, feed_dict = {x: batch_xs, y_true: batch_ys})
+        batch_losses.append(sess.run(loss, feed_dict = {x: batch_xs, y_true: batch_ys}))
+    #if t%10 ==0:
+    batchTot.append(np.average(batch_losses))
+plot(batchTot)
+show()
 
-batch_number = 250
-num_batch = 0
+print(sess.run(w))
 
-
-for i in range(total_epochs):
-    if  num_batch < batch_number:
-        for y in range(mini_epochs):
-            batch_xs = shuttleData[(0+num_batch*batch_size):(batch_size+num_batch*batch_size)][0:shape[1]]
-            batch_ys = shuttleData[(0+num_batch*batch_size):(batch_size+num_batch*batch_size)][0:shape[1]]
-            sess.run([train], feed_dict = {x: batch_xs, y_true: batch_ys})
-        num_batch += 1
-    else:
-        for j in range(shape[0]- batch_number*batch_size):
-            temp = j + batch_number*batch_size
-            batch_xs = [shuttleData[temp][0:shape[1]]]
-            batch_ys = [shuttleData[temp][0:shape[1]]]
-            sess.run([train], feed_dict = {x: batch_xs, y_true: batch_ys})
-    if i > 250:
-        train_losses.append(sess.run([loss], feed_dict = {x: batch_xs, y_true: batch_ys}))
-
-
-
-#print(train_losses)
-print(np.average(train_losses))
-crazy = []
-mean = np.average(train_losses)
-stdDev = np.std(train_losses)
-print(stdDev)
-
-for i in range(len(train_losses)):
-    if train_losses[i] > mean + 2*stdDev:
-        crazy.append(train_losses[i])
-print(crazy)
-u = 0
-nums = []
-length = len(train_losses)
-while u < length:
-    if train_losses[u] > mean+ 2*stdDev:
-        nums.append(u)
-        del train_losses[u]
-        length -=1
-    u +=1
-mean= np.average(train_losses)
-print(mean)
-
-stdDev = std(train_losses)
-print(stdDev)
-
-threshhold = mean + 2*stdDev
-
-good = 0
-bad = 0
-for j in range(len(train_losses)):
-    if train_losses[j] < threshhold:
-        good +=1
-    else:
-        bad +=1
-print("Good ", good)
-print("Bad ", bad)
-print(nums)
+for q in range(200):
+    losses = []
+    for t in range(shape[0]-1):
+        temp = randint(0,shape[0]-1) 
+        batch_xs = [shuttleData[temp][0:shape[1]]]
+        batch_ys = [shuttleData[temp][0:shape[1]]]
+        sess.run(optimizer, feed_dict = {x: batch_xs, y_true: batch_ys})
+        losses.append(sess.run(loss, feed_dict = {x: batch_xs, y_true: batch_ys}))
+    print(np.average(losses))
 
 
-print("Going into Testing")
+print(sess.run(w))
 
-'''
+
 test_losses = []
-for u in range(goodShape[0]):
-    bobweir = np.array([goodTest[u][:]])
-    test_act = np.array([goodTest[u][:]])
+print("")
+print("Testing....")
+print("")
+
+for u in range(300):
+    bobweir = np.array([shuttleTest[u][:]])
+    test_act = np.array([shuttleTest[u][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
     check =tf.sigmoid(tf.matmul(bobweir, w)+b)
     check2 = tf.matmul(check, wo)+bo
     test_losses.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-    sess.run([train, loss], feed_dict = {x: bobweir, y_true: test_act})
     
 
-
-stdDev = np.std(test_losses)
 mean = np.average(test_losses)
-threshhold = 2*stdDev + mean
-print("Mean = ", mean)
-print("Std Dev = ", stdDev)
+print("Class 1 Average Loss ", mean)
+print("")
+
 
 test_losses2 = []
-for p in range(shapeTest[0]):
-    bobweir = np.array([shuttleTest[p][:]])
-    test_act = np.array([shuttleTest[p][:]])
+for l in range(shapeTest2[0]):
+    bobweir = np.array([shuttleTest2[l][:]])
+    test_act = np.array([shuttleTest2[l][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
     check =tf.sigmoid(tf.matmul(bobweir, w)+b)
     check2 = tf.matmul(check, wo)+bo
     test_losses2.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
 
-#plot(test_losses)
-#show()
-u = 0
-outliers = 0
-length = len(test_losses)
-while u < length:
-    if test_losses[u] > .5*stdDev + mean:
-        del test_losses[u]
-        length -=1
-        outliers += 1
-    u +=1
+mean =(np.average(test_losses2))
 
-print("Number of Outliers ", outliers)
-stdDev = np.std(test_losses)
-print("\n")
-print("After Removing Outliers")
-print("Std Benign Loss ", stdDev)
-print("\n")
-mean = np.average(test_losses)
-threshhold = 2*stdDev + mean
+print("Class 2 Average Loss ", mean)
+print("")
+
+test_losses3 = []
+for l in range(shapeTest3[0]):
+    bobweir = np.array([shuttleTest3[l][:]])
+    test_act = np.array([shuttleTest3[l][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
+    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
+    check2 = tf.matmul(check, wo)+bo
+    test_losses3.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
+
+mean =(np.average(test_losses3))
 
 
+print("Class 3 Average Loss ", mean)
+print("")
 
-good = 0
-bad = 0
-badIndex = []
-for n in range (len(test_losses)):
-    if test_losses[n] < threshhold:
-        good +=1
+
+test_losses4 = []
+for z in range(300):
+    bobweir = np.array([shuttleTest4[z][:]])
+    test_act = np.array([shuttleTest4[z][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
+    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
+    check2 = tf.matmul(check, wo)+bo
+    test_losses4.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
+
+mean=(np.average(test_losses4))
+print("Class 4 Average Loss ", mean)
+print("")
+
+test_losses5 = []
+for v in range(300):
+    bobweir = np.array([shuttleTest5[v][:]])
+    test_act = np.array([shuttleTest5[v][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
+    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
+    check2 = tf.matmul(check, wo)+bo
+    test_losses5.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
+
+mean = (np.average(test_losses5))
+print("Class 5 Average ", mean)
+print("")
+
+mean = np.average(test_losses4)
+stdDev = np.std(test_losses4)
+threshhold = mean + stdDev
+
+
+
+test_losses6 = []
+for r in range(shapeTest6[0]):
+    bobweir = np.array([shuttleTest6[r][:]])
+    test_act = np.array([shuttleTest6[r][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
+    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
+    check2 = tf.matmul(check, wo)+bo
+    test_losses6.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
+
+mean = (np.average(test_losses6))
+print("Class 6 Average ", mean)
+print("")
+
+test_losses7 = []
+for s in range(shapeTest7[0]):
+    bobweir = np.array([shuttleTest7[s][:]])
+    test_act = np.array([shuttleTest7[s][:]])
+    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
+    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
+    check2 = tf.matmul(check, wo)+bo
+    test_losses7.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
+
+mean = (np.average(test_losses7))
+print("Class 7 Average ", mean)
+print("")
+
+
+positive = 0
+falsePositive = 0
+negative = 0
+falseNegative = 0
+
+
+for j in range(len(test_losses)):
+    if test_losses[j] > threshhold:
+        falsePositive += 1
     else:
-        bad +=1
-print("Good Test ", good)
-print("GoodTest Bad ", bad)
-good2 = 0
-bad2 = 0
-for a in range (len(test_losses2)):
-    if test_losses2[a] > threshhold:
-        good2 +=1
-    else:
-        bad2 +=1
+        positive += 1
 
-print("Bad Good = " , good2)
-print("Bad Bad = ", bad2)
-'''
+
+
+for i in range(len(test_losses5)):
+    if test_losses5[i] > threshhold:
+        negative += 1
+    else:
+        falseNegative += 1
+for i in range(len(test_losses4)):
+    if test_losses4[i] > threshhold:
+        negative += 1
+    else:
+        falseNegative += 1
+for i in range(len(test_losses3)):
+    if test_losses3[i] > threshhold:
+        negative += 1
+    else:
+        falseNegative += 1
+
+for i in range(len(test_losses2)):
+    if test_losses2[i] > threshhold:
+        negative += 1
+    else:
+        falseNegative += 1
+for i in range(len(test_losses6)):
+    if test_losses6[i] > threshhold:
+        negative += 1
+    else:
+        falseNegative += 1
+for i in range(len(test_losses7)):
+    if test_losses7[i] > threshhold:
+        negative += 1
+    else:
+        falseNegative += 1
+
+print("Positive ", positive)
+print("False Positive ", falsePositive)
+print("Negative ", negative)
+print("False Negative ", falseNegative)
 
