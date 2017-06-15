318712ee83fc70c45196014bb0bf74a805d98b7b
JP <jc7553a@student.american.edu>
2017-06-15 18:09:49 -0400

    Added Change Point Detection

--- stream_autoencoder.py
+++ stream_autoencoder.py
@@ -28,11 +28,150 @@ def normalize(train):
         k +=1
 
     return train
+
+
+def changePoints(mat):
+    delta = 0.3
+
+
+    n = len(mat)
+
+    b = -2
+    c = 2
+    r = 4
+    d = (c-b)/(r-2)
+    eps = .7
+    gamma = 0.1
+
+    xi = np.zeros((n, r))
+
+    B = b*np.ones((n,1))
+    C = c*np.ones((n,1))
+
+
+    temp = np.zeros((n,1))
+
+    for i in range(n):
+        if mat[i] <= B[i][0]:
+            xi[i][0] = 1
+        else:
+            xi[i][0] = 0
+    j = 1
+#while j < r:
+    for h in range(n):
+        if mat[h] > C[h][0]:
+            xi[h][3] = 1
+        else:
+            xi[h][3] = 0
+    #j +=1
+
+    m = 2
+    temp = np.zeros((n,1))
+    temp2 = np.zeros((n,1))
+    temp3 = np.zeros((n,1))
+
+    while m < r:
+        
+        for i in range(n):
+            if mat[i] > B[i][0]+(d*(m-2)):
+                temp[i][0] = 1
+            else:
+                temp[i][0] = 0
+        for j in range(n):
+            if mat[j] <= B[i][0] +(d*(m-1)):
+                temp2[j][0] = 1
+            else:
+                temp2[j][0] = 0
+    #print(temp2)
+        for t in range(n):
+            temp3[t][0] = temp[t][0]*temp2[t][0]
+        for q in range(n):
+            xi[q][m-1] = temp3[q][0]
+        m = m+1
+
+    #print(xi)
+
+    k = 1
+    p = np.zeros((1,r))
+    q = np.zeros((1,r))
+    S = []
+
+    while k < n-3:
+        mean = 0
+        'Find P'
+        for i in range(r):
+            for h in range(k):
+                mean += xi[h][i]
+            mean = mean/k
+            p[0][i] = mean
+
+        'Find q'
+        mean = 0
+        for j in range(r):
+            beb = k
+            while beb < n:
+                mean += xi[beb][j]
+                beb +=1
+            mean = mean/(n-k)
+            q[0][j] = mean
+        #if k ==2:
+            #print(p)
+        'Find u'
+        u = 0
+        for i in range(r):
+            if q[0][i] == 0:
+                u+=1
+        ep = eps/(n-k)
+
+        z = np.zeros((r,1))
+    #S = np.zeros(((n-3),1))
+        for m in range(r):
+            if p[0][m] > 0:
+                if u == 0:
+                    z[m][0] = p[0][m] *np.log(p[0][m]/q[0][m])
+                elif q[0][m] == 0:
+                    z[m][0] = p[0][m]*np.log(p[0][m]/ ep*u)
+                else:
+                    z[m][0] = p[0][m] *np.log(p[0][m]/q[0][m]/(1-ep))
+            adder = 0
+        for u in range(r):
+            adder += z[u][0]
+        S.append(adder) 
+        k +=1
+
+
+    margin = gamma*n
+    t = int(margin)
+    temp = -1
+    while t < int(n-margin):
+        if S[t] > temp:
+            temp = S[t]
+        t +=1
+    print(temp)
+    
+    Smax = max(S[(int(margin)):(int(n-margin))])
+    k = (int (margin))
+    while k < (int(n-margin)):
+        if S[k] > Smax -0.0000001:
+            nuhat = k
+        k +=1
+    print("NUHAT")
+    print(nuhat)
+    W = []
+    for i in range(n):
+        W.append(0)
+
+    for i in range(800):
+        W[i+100] = max([0, W[i+99]+S[i+100]-S[i+99]])
+
+    plot(W)
+    show()
+        
     
 print("Training on Class 1 ")
 
 mat =[]
-for line in open('shuttleTrain1.txt').readlines():
+for line in open('shuttleTrain.txt').readlines():
     holder = line.split(' ')
     i = 0
     for i in range (len(holder)):
@@ -58,7 +197,7 @@ shape = np.shape(shuttleData)
 
 
 mat2 =[]
-for line in open('shuttleTest1.txt').readlines():
+for line in open('shuttleTest.txt').readlines():
     holder = line.split(' ')
     i = 0
     for i in range (len(holder)):
@@ -121,6 +260,8 @@ shapeTest4 = np.shape(class4)
 shapeTest5 = np.shape(class5)
 shapeTest6 = np.shape(class6)
 shapeTest7 = np.shape(class7)
+
+
 
 
 shuttleTest = np.array(class1[0:shapeTest[0]][:]).astype('float32')
@@ -201,7 +342,7 @@ for t in range(200):
 
 
 print("Start Online Training....")
-for q in range(6):
+for q in range(4):
     losses = []
     for t in range(shape[0]-1):
         temp = randint(0,shape[0]-1) 
@@ -218,7 +359,7 @@ print("")
 print("Testing....")
 print("")
 
-for u in range(300):
+for u in range(1000):
     bobweir = np.array([shuttleTest[u][:]])
     test_act = np.array([shuttleTest[u][:]])
     #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
@@ -259,7 +400,7 @@ print("")
 '''
 
 test_losses4 = []
-for z in range(300):
+for z in range(2):
     bobweir = np.array([shuttleTest4[z][:]])
     test_act = np.array([shuttleTest4[z][:]])
     check =tf.sigmoid(tf.matmul(bobweir, w)+b)
@@ -271,7 +412,7 @@ print("Class 4 Average Loss ", mean)
 print("")
 '''
 test_losses5 = []
-for v in range(400):
+for v in range(2):
     bobweir = np.array([shuttleTest5[v][:]])
     test_act = np.array([shuttleTest5[v][:]])
     check =tf.sigmoid(tf.matmul(bobweir, w)+b)
@@ -312,8 +453,11 @@ positive = 0
 falsePositive = 0
 negative = 0
 falseNegative = 0
+mean = 0
+for i in range(len(test_losses)):
+    mean = mean + test_losses[i]
 
-mean = np.average(test_losses)
+mean = mean /len(test_losses)
 stdDev = np.std(test_losses)
 threshhold = mean + 1.25*stdDev
 
@@ -322,34 +466,34 @@ conf = []
 
 for j in range(len(test_losses)):
     if test_losses[j] > threshhold:
-        falsePositive += 1
+        falseNegative += 1
         conf.append(1)
     else:
         positive += 1
         conf.append(0)
 
 
-for i in range(len(test_losses5)):
-    if test_losses5[i] > threshhold:
+for i in range(len(test_losses4)):
+    if test_losses4[i] > threshhold:
         negative +=1
         conf.append(1)
     else:
-        falseNegative += 1
+        falsePositive += 1
         conf.append(0)
         
-for i in range(len(test_losses4)):
-    if test_losses4[i] > threshhold:
+for i in range(len(test_losses5)):
+    if test_losses5[i] > threshhold:
         negative += 1
         conf.append(1)
     else:
-        falseNegative += 1
+        falsePositive += 1
         conf.append(0)
 for i in range(len(test_losses3)):
     if test_losses3[i] > threshhold:
         negative += 1
         conf.append(1)
     else:
-        falseNegative += 1
+        falsePositive += 1
         conf.append(0)
 
 for i in range(len(test_losses2)):
@@ -357,14 +501,14 @@ for i in range(len(test_losses2)):
         negative += 1
         conf.append(1)
     else:
-        falseNegative += 1
+        falsePositive += 1
         conf.append(0)
 for i in range(len(test_losses6)):
     if test_losses6[i] > threshhold:
         negative += 1
         conf.append(1)
     else:
-        falseNegative += 1
+        falsePositive += 1
         conf.append(0)
         
 for i in range(len(test_losses7)):
@@ -372,13 +516,13 @@ for i in range(len(test_losses7)):
         negative += 1
         conf.append(1)
     else:
-        falseNegative += 1
+        falsePositive += 1
         conf.append(0)
 
 acts = []
-for i in range(len(test_losses5)):
+for i in range(len(test_losses)):
     acts.append(0)
-longL = len(test_losses)+len(test_losses2)+len(test_losses3)+len(test_losses4)+len(test_losses6)+len(test_losses7)
+longL = len(test_losses4)+len(test_losses2)+len(test_losses3)+len(test_losses5)+len(test_losses6)+len(test_losses7)
 for j in range(longL):
     acts.append(1)
 '''
@@ -392,8 +536,16 @@ pos = negative + positive
 acc = float(pos/tot)
 y_pred = pd.Series(conf, name = 'Predicted')
 y_act = pd.Series(acts, name = 'Actual')
-df_confusion = pd.crosstab(y_act, y_pred, rownames = ['Actual'], colnames = ['Predicted'], margins = True)
+df_confusion = pd.crosstab(y_pred, y_act, rownames = ['Actual'], colnames = ['Predicted'], margins = True)
 print(df_confusion)
-
+print("")
+print("True Positive ", positive)
+print("False Positive ", falsePositive)
+print("True Negative ", negative)
+print("False Negative ", falseNegative)
+print("")
 print("Accuracy % = ", acc*100)
-
+print("")
+changePoints(test_losses)
+print("")
+print(test_losses)
