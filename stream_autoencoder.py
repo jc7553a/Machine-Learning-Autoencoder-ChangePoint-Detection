b738f949568826e69b28cad74bcc3927d0d07e4b
JP <jc7553a@student.american.edu>
2017-06-20 16:44:50 -0400

    Autoencoder is now a class

--- stream_autoencoder.py
+++ stream_autoencoder.py
@@ -1,16 +1,79 @@
 import tensorflow as tf
 import numpy as np
-import pylab
 from random import randint
-#import scipy
 from matplotlib.pylab import *
 import pandas as pd
 
+class Autoencoder(object):
+
+    def __init__(self, n_features, n_hidden):
+        self.n_features = n_features
+        self.n_hidden = n_hidden
+        #self.transfer = transfer_function
+
+        network_weights = self._initialize_weights()
+        self.weights = network_weights
+
+        # model
+        self.x = tf.placeholder(tf.float32, [None, self.n_features])
+        self.hidden = tf.sigmoid(tf.add(tf.matmul(self.x, self.weights['w1']), self.weights['b1']))
+        self.reconstruction = tf.add(tf.matmul(self.hidden, self.weights['w2']), self.weights['b2'])
+
+        # cost
+        self.cost = tf.reduce_mean(tf.square(self.reconstruction - self.x))
+        self.optimizer = tf.train.GradientDescentOptimizer(0.05).minimize(self.cost)
+
+        init = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(init)
+        'Uncomment to Look at Graph and Nodes'
+        #print(tf.get_default_graph().as_graph_def())
+
+
+    def _initialize_weights(self):
+        all_weights = dict()
+        all_weights['w1'] = tf.Variable(tf.truncated_normal([self.n_features, self.n_hidden], stddev = .001), name = 'weights1')
+        all_weights['b1'] = tf.Variable(tf.truncated_normal([self.n_hidden], stddev = .001), name = 'bias1')
+        all_weights['w2'] = tf.Variable(tf.truncated_normal([self.n_hidden, self.n_features], stddev = .001), name = 'weights_o')
+        all_weights['b2'] = tf.Variable(tf.truncated_normal([self.n_features], stddev = .001), name = 'biases_o')
+        return all_weights
+
+    def partial_fit(self, X):
+        cost, opt = self.sess.run((self.cost, self.optimizer), feed_dict={self.x: X})
+        return cost
+
+    def calc_total_cost(self, X):
+        return self.sess.run(self.cost, feed_dict = {self.x: X})
+
+    def transform(self, X):
+        return self.sess.run(self.hidden, feed_dict={self.x: X})
+
+    def generate(self, hidden = None):
+        if hidden is None:
+            hidden = self.sess.run(tf.random_normal([1, self.n_hidden]))
+        return self.sess.run(self.reconstruction, feed_dict={self.hidden: hidden})
+
+    def reconstruct(self, X):
+        return self.sess.run(self.reconstruction, feed_dict={self.x: X})
+
+    def getWeights(self):
+        return self.sess.run(self.weights['w1'])
+
+    def getBiases(self):
+        return self.sess.run(self.weights['b1'])
+    
+    def getHiddenWeights(self):
+        return self.sess.run(self.weights['w2'])
+    
+    def getHiddenBiases(self):
+        return self.sess.run(self.weights['b2'])
+
+    'Uncomment to Look at Graph and Nodes'
+    #print(tf.get_default_graph().as_graph_def())
 
 def normalize(train):
     train = np.array(train)
     shape = np.shape(train)
-    
     k = 0
     while k <shape[1]:
         maxim = 0
@@ -26,16 +89,12 @@ def normalize(train):
         for t in range(shape[0]):
             train[t][k] = (train[t][k] - minim)/denom
         k +=1
-
     return train
 
 
 def changePoints(mat):
     delta = 0.3
-
-
     n = len(mat)
-
     b = -2
     c = 2
     r = 4
@@ -164,14 +223,14 @@ def changePoints(mat):
     for i in range(800):
         W[i+100] = max([0, W[i+99]+S[i+100]-S[i+99]])
 
-    plot(W)
-    show()
-        
-    
+    #plot(W)
+    #show()
+
+
 print("Training on Class 1 ")
 
 mat =[]
-for line in open('shuttleTrain.txt').readlines():
+for line in open('shuttleTrain1.txt').readlines():
     holder = line.split(' ')
     i = 0
     for i in range (len(holder)):
@@ -197,7 +256,7 @@ shape = np.shape(shuttleData)
 
 
 mat2 =[]
-for line in open('shuttleTest.txt').readlines():
+for line in open('shuttleTest1.txt').readlines():
     holder = line.split(' ')
     i = 0
     for i in range (len(holder)):
@@ -272,85 +331,42 @@ shuttleTest5 = np.array(class5[0:shapeTest5[0]][:]).astype('float32')
 shuttleTest6 = np.array(class6[0:shapeTest6[0]][:]).astype('float32')
 shuttleTest7 = np.array(class7[0:shapeTest7[0]][:]).astype('float32')
 
-
-
-
-n_hidden = 5
-n_features = shape[1]
-
-
-
-x = tf.placeholder(tf.float32 , [None , n_features], name = 'x')
-#learnRate = tf.placeholder(tf.float32, shape = [], name = 'LR')
-
-'Weights and Biases to Hidden Layer'
-
-w = tf.Variable(tf.truncated_normal([n_features ,n_hidden], stddev = .001), name = 'weights_h')
-b = tf.Variable(tf.truncated_normal([n_hidden], stddev = .001), name = 'biases_h')
-
-'Weights and Biases to Output Layer'
-wo = tf.Variable(tf.truncated_normal([n_hidden, n_features],stddev = .001), name = 'weights_o')
-bo = tf.Variable(tf.truncated_normal([n_features], stddev = .001), name = 'biases_o')
-
-'Calculations for Encoder and Decoder'
-encoder = tf.nn.sigmoid(tf.matmul(x, w) + b)
-#hidden1 = tf.nn.sigmoid(tf.matmul(encoder, w2)+b2)
-#hidden2 = tf.nn.sigmoid(tf.matmul(hidden1, w3)+b3)
-decoder = tf.matmul(encoder, wo) +bo
-y = decoder
-
-
-
-'Objective Functions'
-y_true = tf.placeholder(tf.float32, [None, n_features], name = 'y_true')
-loss = tf.reduce_mean(tf.square(y_true - y))
-optimizer = tf.train.GradientDescentOptimizer(.05).minimize(loss)
-#train = optimizer.minimize(loss)
-
-
-
-'Uncomment to Look at Graph and Nodes'
-#print(tf.get_default_graph().as_graph_def())
-
-losses = []
-mini_epochs = 400
-batch_size = 5
-
-
-'Load Graph'
-init = tf.global_variables_initializer()
-sess = tf.Session()
-sess.run(init)
-
-
-
+myAuto = Autoencoder(shape[1], 5)
 train_losses = []
 print("Start Training Batches....")
 batchTot = []
+mini_epochs = 400
+batch_size = 5
+
 for t in range(200):
     batch_losses = []
     for y in range(mini_epochs):
         rando = randint(0,shape[0]-5)
         batch_xs = shuttleData[rando:rando+batch_size][0:shape[1]]
-        batch_ys = shuttleData[(rando):(rando+batch_size)][0:shape[1]]
-        sess.run(optimizer, feed_dict = {x: batch_xs, y_true: batch_ys})
-        batch_losses.append(sess.run(loss, feed_dict = {x: batch_xs, y_true: batch_ys}))
-    if t%10 ==0:
-        batchTot.append(np.average(batch_losses))
+        batch_losses.append(myAuto.partial_fit(batch_xs))
+    #if t%10 ==0:
+    batchTot.append(np.average(batch_losses))
+    #print(batchTot[t%10])
 #plot(batchTot)
 #show()
 
 
-print("Start Online Training....")
-for q in range(4):
-    losses = []
+for q in range(6):
+    #losses = []
     for t in range(shape[0]-1):
         temp = randint(0,shape[0]-1) 
         batch_xs = [shuttleData[temp][0:shape[1]]]
-        batch_ys = [shuttleData[temp][0:shape[1]]]
-        sess.run(optimizer, feed_dict = {x: batch_xs, y_true: batch_ys})
-        losses.append(sess.run(loss, feed_dict = {x: batch_xs, y_true: batch_ys}))
-   # print(np.average(losses))
+        #losses.append(myAuto.partial_fit(batch_xs))
+    #print(np.average(losses))
+#plot(losses)
+#show()
+losses = []
+for i in range(4000):
+    batch_xs = [shuttleData[i][0:shape[1]]]
+    losses.append(myAuto.calc_total_cost(batch_xs))
+
+cp = changePoints(losses)
+
 
 
 
@@ -359,95 +375,50 @@ print("")
 print("Testing....")
 print("")
 
-for u in range(1000):
+for u in range(200):
     bobweir = np.array([shuttleTest[u][:]])
-    test_act = np.array([shuttleTest[u][:]])
-    #sess.run([train], feed_dict = {x: bobweir, y_true: test_act})
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-    
-'''
-mean = np.average(test_losses)
-print("Class 1 Average Loss ", mean)
-print("")
-'''
+    test_losses.append(myAuto.calc_total_cost(bobweir))
+
 
 test_losses2 = []
 for l in range(shapeTest2[0]):
     bobweir = np.array([shuttleTest2[l][:]])
-    test_act = np.array([shuttleTest2[l][:]])
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses2.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-'''
-mean =(np.average(test_losses2))
+    test_losses2.append(myAuto.calc_total_cost(bobweir))
 
-print("Class 2 Average Loss ", mean)
-print("")
-'''
+
+
 test_losses3 = []
 for l in range(shapeTest3[0]):
     bobweir = np.array([shuttleTest3[l][:]])
-    test_act = np.array([shuttleTest3[l][:]])
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses3.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-'''
-mean =(np.average(test_losses3))
-print("Class 3 Average Loss ", mean)
-print("")
-'''
+    test_losses3.append(myAuto.calc_total_cost(bobweir))
+
+
 
 test_losses4 = []
-for z in range(2):
+for z in range(200):
     bobweir = np.array([shuttleTest4[z][:]])
-    test_act = np.array([shuttleTest4[z][:]])
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses4.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-'''
-mean=(np.average(test_losses4))
-print("Class 4 Average Loss ", mean)
-print("")
-'''
+    test_losses4.append(myAuto.calc_total_cost(bobweir))
+
+
 test_losses5 = []
-for v in range(2):
+for v in range(200):
     bobweir = np.array([shuttleTest5[v][:]])
-    test_act = np.array([shuttleTest5[v][:]])
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses5.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-'''
-mean = (np.average(test_losses5))
-print("Class 5 Average ", mean)
-print("")
-'''
+    test_losses5.append(myAuto.calc_total_cost(bobweir))
+
+
 
 test_losses6 = []
 for r in range(shapeTest6[0]):
     bobweir = np.array([shuttleTest6[r][:]])
-    test_act = np.array([shuttleTest6[r][:]])
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses6.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-'''
-mean = (np.average(test_losses6))
-print("Class 6 Average ", mean)
-print("")
-'''
+    test_losses6.append(myAuto.calc_total_cost(bobweir))
+
+
+
 test_losses7 = []
 for s in range(shapeTest7[0]):
     bobweir = np.array([shuttleTest7[s][:]])
-    test_act = np.array([shuttleTest7[s][:]])
-    check =tf.sigmoid(tf.matmul(bobweir, w)+b)
-    check2 = tf.matmul(check, wo)+bo
-    test_losses7.append(sess.run(tf.reduce_sum(tf.square(check2 - test_act))))
-'''
-mean = (np.average(test_losses7))
-print("Class 7 Average ", mean)
-print("")
-'''
+    test_losses7.append(myAuto.calc_total_cost(bobweir))
+
 
 positive = 0
 falsePositive = 0
@@ -459,9 +430,10 @@ for i in range(len(test_losses)):
 
 mean = mean /len(test_losses)
 stdDev = np.std(test_losses)
-threshhold = mean + 1.25*stdDev
+threshhold = mean+1.5*stdDev
 
 conf = []
+
 
 
 for j in range(len(test_losses)):
@@ -525,12 +497,13 @@ for i in range(len(test_losses)):
 longL = len(test_losses4)+len(test_losses2)+len(test_losses3)+len(test_losses5)+len(test_losses6)+len(test_losses7)
 for j in range(longL):
     acts.append(1)
-'''
+
+
 print("Positive ", positive)
 print("False Positive ", falsePositive)
 print("Negative ", negative)
 print("False Negative ", falseNegative)
-'''
+
 tot = negative+positive +falsePositive + falseNegative
 pos = negative + positive
 acc = float(pos/tot)
@@ -546,6 +519,5 @@ print("False Negative ", falseNegative)
 print("")
 print("Accuracy % = ", acc*100)
 print("")
-changePoints(test_losses)
-print("")
-print(test_losses)
+
+
